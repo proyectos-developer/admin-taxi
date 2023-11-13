a4bf584d4cc6f93a335fe0cf17218c5f4ca322cf
proyectos-developer <proyectos@developer-ideas.com>
2023-11-13 11:59:29 -0500

    V1.0.2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require ('express')
+const path = require('path');
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get ('/api', (req, res) => {
     res.json ({
@@ -13,3 +16,8 @@ app.get ('/api', (req, res) => {
 app.listen (PORT, () => {
     console.log (`Servidor escuchando el puerto ${PORT}`)
 })
+
+// Todas las peticiones GET que no hayamos manejado en las líneas anteriores retornaran nuestro app React
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
